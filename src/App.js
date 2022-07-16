513f7a01ea9577bd10b4a38ec83f5c270a8726a7
malfell <malfellows13@gmail.com>
2022-07-16 17:36:28 -0500

    added general loading text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+// Import Suspense
+import React, { Suspense} from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+// imported with lazy loading
+const AboutUs = React.lazy( () => import('./components/AboutUs'))
+const Packages = React.lazy( () => import('./components/Packages'))
 
 function App() {
   return (
@@ -9,8 +11,11 @@ function App() {
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
-          <AboutUs />
-          <Packages />
+        <Suspense fallback={<h1>Loading...</h1>}>
+            <AboutUs />
+            <Packages />
+        </Suspense>
+
       </div>
     </div>
   );

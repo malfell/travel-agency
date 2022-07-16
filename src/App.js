930ef196d9c773f016e9f6597c1d889dc98fb0aa
malfell <malfellows13@gmail.com>
2022-07-16 17:50:11 -0500

    did bonus and added spinner image

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 // Import Suspense
 import React, { Suspense} from 'react';
 import './App.css';
+// import loading spinner
+// install it in terminal with: npm install react-loader-spinner
+//https://www.npmjs.com/package/react-loader-spinner
+import { TailSpin } from 'react-loader-spinner';
+
+
+function App() {
 // imported with lazy loading
 const AboutUs = React.lazy( () => import('./components/AboutUs'))
 const Packages = React.lazy( () => import('./components/Packages'))
 
-function App() {
+
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
-        <Suspense fallback={<h1>Loading...</h1>}>
+        {/* suspense fallback first loading text*/}
+        {/* <Suspense fallback={<h1>Loading...</h1>}>
             <AboutUs />
             <Packages />
-        </Suspense>
+        </Suspense> */}
+        {/* suspense fallback loading spinner */}
+        <Suspense fallback={<TailSpin color="#00BFFF" height={80} width={80} style={{margin: '0 auto'}} />}>
+            <AboutUs />
+            <Packages />
+        </Suspense>        
 
       </div>
     </div>
